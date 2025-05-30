9cb3a1f1534625f46655e7d65399477eef67c3bf
ArunPragas2001 <pragasarun1@gmail.com>
2025-05-30 10:32:01 +0000

    123

---
+++ java/DAY12/DAY15/Park.java
@@ -0,0 +1,62 @@
+class Vehicle{
+       // define private fields;
+    private String LicensePlate;
+    private String OwnerName;
+    private int Parked;
+
+       // constructor
+    vehicle(String LicensePlate,String OwnerName,int hoursparked ){
+     this.LicensePlate=LicensePlate;
+     this.OwnerName=OwnerName;
+     this hoursparked=hoursparked;
+             
+       }
+   
+   //getters
+       public String getLicenseplate()
+       {
+            return LicensePlate;
+       }
+
+        public String getOwnerName()
+       {
+            return OwnerName;
+       }
+        
+        public int getHoursParked()
+        {
+            return hoursparked;
+        }
+
+
+       //setters
+
+       public void setLicenseplate(String LicencePlate)
+       {
+        this.LicencePlate=LicensePlate;
+       }
+       
+       public void setOwnerName(String OwnerName)
+       {
+        this.OwnerName=OwnerName;
+       } 
+
+       public void setHoursParked(int  hoursparked)
+        {
+
+            this.hoursparked=hoursparked;
+        }
+
+
+
+}
+
+class ParkingLot{
+    
+    Vehicle vehi[];
+    int VehiCount=0;
+
+    vehi[] vehicles=new vehi[5];
+
+
+}
